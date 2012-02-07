f2a207bc315a5458837a12899ffecf295eacd413
K900 <k0009000@gmail.com>
2012-02-07 22:06:48 +0400

    Add legacy libgralloc (for bravo)

--- Android.mk
+++ Android.mk
@@ -1,8 +1,12 @@
 #libs to be built for QCOM targets only
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-    display-hals := libqcomui libtilerenderer
-    display-hals += libhwcomposer liboverlay libgralloc libgenlock libcopybit
-    include $(call all-named-subdir-makefiles,$(display-hals))
+    ifeq ($(BOARD_USES_LEGACY_QCOM),true)
+        display-hals := libgralloc-legacy libcopybit
+    else
+        display-hals := libqcomui libtilerenderer
+        display-hals += libhwcomposer liboverlay libgralloc libgenlock libcopybit
+    endif
+        include $(call all-named-subdir-makefiles,$(display-hals))
 endif
 
