1f72bc69a2e7205f767d9a2ca3139712fd293243
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-02-14 20:45:54 -0600

    qcom display: qsd8k hwcomposer
    hwcomposer:
             rewrite from default template.
             specific for qsd8k
             put a hack in to fix screen off animation
             builds without liboverlay include which qsd8k doesnt support
    
    Change-Id: Ica8df89be4dba4e92ed4e879e8e821b841e2ef93

--- Android.mk
+++ Android.mk
@@ -3,6 +3,9 @@
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     ifeq ($(BOARD_USES_LEGACY_QCOM),true)
         display-hals := libgralloc-legacy libcopybit
+    else ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+        display-hals := libhwcomposer-qsd8k libgralloc libcopybit libgenlock
+        display-hals += libtilerenderer libqcomui
     else
         display-hals := libqcomui libtilerenderer
         display-hals += libhwcomposer liboverlay libgralloc libgenlock libcopybit
